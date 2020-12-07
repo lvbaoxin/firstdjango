6aaee6f21aee844c2b55833b9132e90a33b8668b
lvbaoxin <745458741@qq.com>
2020-12-07 17:21:29 +0800

    新加详情，搜索，公共页面。

--- lyu/views.py
+++ lyu/views.py
@@ -1,5 +1,24 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
 def index(request):
-    return render(request,'index.html',{})
+    import requests
+    import json
+    api_request = requests.get("https://api.github.com/users?since=0")
+    api = json.loads(api_request.content)
+    return render(request, 'index.html', {"api": api})
+
+
+def user(request):
+    if request.method == 'POST':
+        import requests
+        import json
+        user = request.POST['user']
+        user_request = requests.get("https://api.github.com/users/" + user)
+        username = json.loads(user_request.content)
+        return render(request, 'user.html', {"user": user, 'username': username})
+    else:
+        notfound = "请在搜索框中输入您需要查询的用户。"
+        return render(request, 'user.html', {'notfound': notfound})
